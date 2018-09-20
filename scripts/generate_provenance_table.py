2a77ca166255f198bb6330e49ec6618781f14eed
Ben Evans <benjamin.robert.evans@gmail.com>
2018-09-20 13:24:05 -0400

    updated generate_provenance_table.py to use manual entries from manual_taxonomy_map.tsv

--- scripts/generate_provenance_table.py
+++ scripts/generate_provenance_table.py
@@ -5,11 +5,12 @@ import re
 import os
 import sys
 import glob
+import urllib
 import pickle
 from nexus import NexusReader # https://pypi.org/project/python-nexus/
 from Bio import SeqIO
 from Bio import Entrez
-Entrez.email = 'casey.dunn@yale.edu'
+Entrez.email = 'b.evans@yale.edu'
 
 def get_names(file_name):
     """read in a phy or nex file, return the taxa names found"""
@@ -28,6 +29,9 @@ def get_names(file_name):
 
 def get_tax_id(query_name):
     """sanitize query name, search for and return NCBI ID"""
+    query_name = query_name.rstrip('_.')
+    if query_name.endswith('_sp'):
+        query_name[:-3]
     query_term = re.sub('[ _-]', '+', query_name)
     search = Entrez.esearch(term = query_term, db = "taxonomy", retmode = "xml")
     record = Entrez.read(search)
@@ -42,47 +46,67 @@ def get_tax_data(tax_id):
     """Fetch taxonomy info with NCBI ID"""
     if tax_id == 'nan':
         return [{'Lineage':'na'}]
-    search = Entrez.efetch(id = tax_id, db = "taxonomy", retmode = "xml")
-    return Entrez.read(search)
-
-# be kind to API, don't ask for what we already know
-if os.path.isfile('known_ids.pickle'):
-    known_ids = pickle.load(open('known_ids.pickle'))
-else:
-    known_ids = {} 
-
-if os.path.isfile('known_tax.pickle'):
-    known_tax = pickle.load(open('known_tax.pickle'))
-else:
-    known_tax = {} 
-
-table_columns = ["original_matrix", "matrix_name", "ncbi_tax_id", 
-                 "ncbi_taxonomy", "relabelled_name", "clade_assignment"]
-table_rows = []
-
-# glob hack to match nex and phy
-for file_name in glob.glob('../raw_data/**/*.[pn][eh][xy]', recursive=True):
-    names_list = []
-    print("Reading file", file_name)
     try:
-        names_list = get_names(file_name)
+        search = Entrez.efetch(id = tax_id, db = "taxonomy", retmode = "xml")
+        return Entrez.read(search)
     except Exception as e:
-        print("Error loading", file_name, ":\n", e)
-    for name in names_list:
-        if name not in known_ids:
-            known_ids[name] = get_tax_id(name) 
-            known_tax[known_ids[name]] = get_tax_data(known_ids[name])[0]
+        print("taxonomy lookup error: ", e)
+    return [{'Lineage':'na'}]
 
-        table_rows.append([ file_name, name, known_ids[name],
-                            known_tax[known_ids[name]]['Lineage'],
-                            name, name])
+def get_manual_entries():
+    fh = open('manual_taxonomy_map.tsv')
+    fh.readline() # discard header
+    manual = dict()
+    for line in fh:
+        tmp = line.rstrip().split()
+        manual[tmp[1]] = tmp[2]
+    return manual
 
-pickle.dump(known_ids, open('known_ids.pickle', 'wb'))
-pickle.dump(known_tax, open('known_tax.pickle', 'wb'))
+def get_pickles():
+    """be kind to API, don't ask for what we already know"""
+    if os.path.isfile('known_ids.pickle'):
+        k_ids = pickle.load(open('known_ids.pickle', 'rb'))
+    else:
+        k_ids = {} 
 
-# print to table file
-sep = '\t'
-table_out = open("taxon_table.tsv", 'w')
-print(sep.join(table_columns), file=table_out)
-for row in table_rows:
-    print(sep.join(row), file=table_out)
+    if os.path.isfile('known_tax.pickle'):
+        k_tax = pickle.load(open('known_tax.pickle', 'rb'))
+    else:
+        k_tax = {}
+    return k_ids, k_tax
+
+if __name__ == '__main__':
+
+    known_ids, known_tax = get_pickles()
+    manual_entries = get_manual_entries()
+    table_columns = ["original_matrix", "matrix_name", "ncbi_tax_id", 
+                     "ncbi_taxonomy", "relabelled_name", "clade_assignment"]
+    sep = '\t'
+    table_out = open("taxon_table.tsv", 'w')
+    print(sep.join(table_columns), file=table_out)
+
+    # glob hack to match nex and phy
+    for file_name in glob.glob('../considered_data/**/*.[pn][eh][xy]', recursive=True):
+        names_list = []
+        print("Reading file", file_name)
+        try:
+            names_list = get_names(file_name)
+        except Exception as e:
+            print("Error loading", file_name, ":\n", e)
+        for original_name in names_list:
+            manual_name = ''
+            if original_name in manual_entries:
+                manual_name = manual_entries[original_name]
+                known_ids[original_name] = get_tax_id(manual_name)
+            elif original_name not in known_ids:
+                known_ids[original_name] = get_tax_id(original_name)
+
+            known_tax[known_ids[original_name]] = get_tax_data(known_ids[original_name])[0]
+
+            print(sep.join([ file_name, original_name, known_ids[original_name],
+                                known_tax[known_ids[original_name]]['Lineage'],
+                                manual_name, original_name]), file=table_out)
+
+    table_out.close()
+    pickle.dump(known_ids, open('known_ids.pickle', 'wb'))
+    pickle.dump(known_tax, open('known_tax.pickle', 'wb'))
