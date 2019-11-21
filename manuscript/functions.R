f8ac38a32c86201bfe1176dc0a964c5b9073a241
Ben Evans <benjamin.robert.evans@gmail.com>
2019-11-21 14:40:23 -0500

    pass at au test / phylogenetic signal plot

--- manuscript/functions.R
+++ manuscript/functions.R
@@ -521,7 +521,7 @@ get_matrix_occupancy = function( seq_matrix ){
 parse_au_gene_tests = function(path="../trees_new/AU_test"){
 	files_list = list.files(path, pattern = "genewise", full.names = TRUE, recursive = TRUE) 
 	filename_parts_list = str_split(gsub('_genewise_lnL.txt', '', basename( files_list )), '_', n=2)
-
+	likelihood_cutoff = 2
 	au_tibble = 
 		files_list %>%
 		map(read.delim) %>%
@@ -530,10 +530,10 @@ parse_au_gene_tests = function(path="../trees_new/AU_test"){
 		bind_rows()
 
 	au_tibble$ABS <- abs(au_tibble$tr1_log.likelihood - au_tibble$tr2_log.likelihood)
-	au_tibble$result[ (au_tibble$tree_supported == "tr1" ) & (au_tibble$ABS >= 1) ] = "Ctenophore-sister-strong-signal"
-	au_tibble$result[ (au_tibble$tree_supported == "tr1" ) & (au_tibble$ABS < 1) ] = "Ctenophore-sister-weak-signal"
-	au_tibble$result[ (au_tibble$tree_supported == "tr2" ) & (au_tibble$ABS >= 1) ] = "Porifera-sister-strong-signal"
-	au_tibble$result[ (au_tibble$tree_supported == "tr2" ) & (au_tibble$ABS <1) ] = "Porifera-sister-weak-signal"
-	
+	au_tibble$result[ (au_tibble$tree_supported == "tr1" ) & (au_tibble$ABS >= likelihood_cutoff) ] = "Ctenophore-sister-strong-signal"
+	au_tibble$result[ (au_tibble$tree_supported == "tr1" ) & (au_tibble$ABS < likelihood_cutoff) ] = "Ctenophore-sister-weak-signal"
+	au_tibble$result[ (au_tibble$tree_supported == "tr2" ) & (au_tibble$ABS >= likelihood_cutoff) ] = "Porifera-sister-strong-signal"
+	au_tibble$result[ (au_tibble$tree_supported == "tr2" ) & (au_tibble$ABS < likelihood_cutoff) ] = "Porifera-sister-weak-signal"
+	au_tibble$result <- factor(au_tibble$result, levels = c("Ctenophore-sister-strong-signal", "Ctenophore-sister-weak-signal", "Porifera-sister-weak-signal", "Porifera-sister-strong-signal"))
 	return(au_tibble)
 }
