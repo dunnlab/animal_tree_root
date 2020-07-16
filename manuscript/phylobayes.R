5eea8e93ff7a2a4706e6b1b7394a95a78876bd2a
Casey Dunn <casey.dunn@yale.edu>
2020-07-16 19:07:49 +0000

    created testing framework and started parsing re #31

---
+++ manuscript/phylobayes.R
@@ -0,0 +1,111 @@
+library(ape)
+
+
+setOldClass("phylo")
+
+#######################################
+## Classes
+
+
+#' An S4 class to store a single sample from a phylobayes chain file
+#' 
+#' @slot allocation  The allocation vector
+setClass(
+  "PhylobayesSample",
+  slots = c(
+    
+    path = "character",
+    generation = "numeric",
+    
+    # These are shown in the order of a phylobayes MPI record. One item per line
+    # unless otherwise noted
+    
+    tree = "phylo",
+    x2 = "numeric", # value 1 appears constant between iterations
+    x3 = "numeric", # could be NSPR kappa or kmax, not in any other file
+    x4 = "numeric", # appears to be alpha parameter, found in trace file
+    x5 = "numeric", # ncat?
+    x6 = "numeric", # 20 numbers, possibly some starting values for each amino acid (last tab is blank)
+    # Blank line
+    frequencies = "numeric", # number of columns is number of states (eg 20 for amino acids), number of rows is the number of categories (one line per category in the file)
+    allocation = "numeric" # integer that indicates the category of each site
+  )
+)
+
+#' Construct a PhylobayesSample from the text for a single generation from a phylobayes chain
+#'
+#' @param sample_text text for a single sample from a phylobayes chain file
+#' @param path path to the original file
+#' @param generation the generation of the sample in the original file
+#' @return A PhylobayesSample object
+#' @export
+PhylobayesSample = function( 
+  text, 
+  path = NA,
+  generation = NA
+  ) {
+  
+  object = new( 
+    "PhylobayesSample",
+    path = path, 
+    generation = generation,
+    tree = ape::read.tree( text=text[1] ),
+    x2 = as.numeric(text[2])
+  )
+  
+  object
+}
+
+#######################################
+## Functions
+
+#' Parse a phylobayes mpi chain file
+#'
+#' @param fine_name name of file to parse
+#' @return A PhylobayesSample object
+#' @export
+
+parse_phylobayes_chain = function( file_name ){
+  
+  generation = 0
+  samples = list()
+  
+  # This will be very inefficient for large files, but will work for a first pass
+  lines = read_lines( file_name )
+  n = 0
+  sample_text = character()
+  
+  for (line in lines) {
+    n = n+1
+    if( n == 1 ) {
+      if ( grepl( "\\(", line ) ) {
+        generation = generation + 1
+        sample_text = append( sample_text, line )
+      } else {
+        stop( "Chain file does not start with a tree" )
+      }
+    } else {
+      if ( grepl( "\\(", line ) ){
+        
+        samples = append( 
+          samples,
+          PhylobayesSample( sample_text, file_name, generation )
+          )
+        
+        # Get ready to parse new record, starting with this line
+        generation = generation + 1
+        sample_text = character()
+      }
+      sample_text = append( sample_text, line )
+    }
+  }
+  
+  # Add the final record
+  samples = append( 
+    samples,
+    PhylobayesSample( sample_text, file_name, generation )
+  )
+  
+  samples
+}
+
--- manuscript/tests/testthat/test_pb.R
+++ manuscript/tests/testthat/test_pb.R
@@ -8,8 +8,32 @@ source( "../../phylobayes.R" )
 
 context("testing framework")
 
+# The example file was gcreated by manually editing 
+# Whelan2017_strict.phy_Poisson_CAT60_Chain_1_last500.chain
+# to obtain the last three generations of the chain
+
+example_name = "example.chain"
+
 test_that("Testing works", {
   expect_equal( 1, 1 )
 })
 
+
+context("single phylobayes chain record")
+
+lines = read_lines( "example_single.chain" )
+
+pb = PhylobayesSample( lines, "example_single.chain", 1 )
+
+test_that("Tree has the correct number of tips", {
+  expect_equal( pb@tree$Nnode, 74 )
+})
+
 context("phylobayes chain files")
+
+chain = parse_phylobayes_chain("example.chain")
+
+test_that("Tree has the correct number of tips", {
+  expect_equal( length(chain), 7 )
+})
+
